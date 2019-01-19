85917e2d757130ea952046eb8a7f23c362652de5
Ryan <ryancurtisclements@gmail.com>
2019-01-19 15:31:44 -0800

    Data Model Created

---
+++ RymeUniversityData/Course.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Course]
+(
+	[CourseID]		INT				IDENTITY(1,1) NOT NULL,
+	[Title]			NVARCHAR(50)	NULL,
+	[Summary]		NVARCHAR(2000)	NULL,
+	[Credits]		INT				NULL,
+	PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
