9a5cd5f50019bc47f21780c060371fef9d799840
Ryan Clements <RyanCurtisClements@users.noreply.github.com>
2019-01-28 14:38:44 -0800

    Implementing Data Validation

--- RymeUniversitySite/Models/RymeModel.Designer.cs
+++ RymeUniversitySite/Models/RymeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\basta\source\repos\RymeUniversitySite\RymeUniversitySite\Models\RymeModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\basta\Documents\GitHub\RymeUniversity\RymeUniversitySite\Models\RymeModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
